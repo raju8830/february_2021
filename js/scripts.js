54acff8cb9608ed26de3dac962eaeed458f955cc
raju8830 <44889262+raju8830@users.noreply.github.com>
2021-02-14 10:43:53 +0600

    february_2021
    february_2021

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,14 @@
 /* About Page Slider*/
 $(document).ready(function(){
+		// Sticky Menu
+	$('.js--sticky-section').waypoint(function(direction){
+		if(direction == 'down'){
+					$('.stiacky_menu').addClass('sticky');
+			 } else {
+					$('.stiacky_menu').removeClass('sticky');
+		}
+	});
+		// Sticky Menu
 	$('#contact_jquery').click(function(){
 		$('#hide_part').hide(1000);
 	});
